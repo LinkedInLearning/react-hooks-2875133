857154282623afe096c868657496f880f111747b
David Lorenz <info@activenode.de>
2021-06-02 18:08:43 +0200

    class 2 functional component

--- src/ListTodos.jsx
+++ src/ListTodos.jsx
@@ -1,40 +1,38 @@
 import React from "react";
 
-export default class ListTodos extends React.Component {
-  render() {
-    return this.props.todos.map((todoObj) => {
-      const { id, title, category, isCompleted, isOptimistic } = todoObj;
+export default function ListTodos(props) {
+  return props.todos.map((todoObj) => {
+    const { id, title, category, isCompleted, isOptimistic } = todoObj;
 
-      return (
-        <tr className={isOptimistic ? "is-optimistic" : ""} key={id}>
-          <td>
-            <label>
-              <input
-                type="checkbox"
-                value={true}
-                checked={isCompleted}
-                onChange={() =>
-                  this.props.onSetCompletionState(todoObj, !isCompleted)
-                }
-              />
+    return (
+      <tr className={isOptimistic ? "is-optimistic" : ""} key={id}>
+        <td>
+          <label>
+            <input
+              type="checkbox"
+              value={true}
+              checked={isCompleted}
+              onChange={() =>
+                props.onSetCompletionState(todoObj, !isCompleted)
+              }
+            />
 
-              <div className="pad-left-10">
-                {isCompleted && <strike>{title}</strike>}
-                {!isCompleted && <strong>{title}</strong>}
-              </div>
-            </label>
-          </td>
-          <td>{category}</td>
-          <td>
-            <button
-              type="button"
-              onClick={() => this.props.onDeleteTodo(todoObj.id)}
-            >
-              löschen
-            </button>
-          </td>
-        </tr>
-      );
-    });
-  }
+            <div className="pad-left-10">
+              {isCompleted && <strike>{title}</strike>}
+              {!isCompleted && <strong>{title}</strong>}
+            </div>
+          </label>
+        </td>
+        <td>{category}</td>
+        <td>
+          <button
+            type="button"
+            onClick={() => props.onDeleteTodo(todoObj.id)}
+          >
+            löschen
+          </button>
+        </td>
+      </tr>
+    );
+  });
 }
