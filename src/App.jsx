ec99a7ec4ce347f343d01552f420f12f602269fb
David Lorenz <info@activenode.de>
2021-06-24 20:25:53 +0200

    revert

--- src/App.jsx
+++ src/App.jsx
@@ -12,30 +12,35 @@ import TodoInput from "./TodoInput";
 import ListTodos from "./ListTodos";
 import { randomId } from "./mock-server/util";
 
-function App() {
-  const [todos, setTodos] = useState([]); // initally empty
+class App extends React.Component {
+  state = {
+    todos: [], // initally empty
+  };
 
-  function updateTodos(updatedTodos) {
-    setTodos(updatedTodos);
-  }
-
-  function componentDidMount() {
-    serverFetchTodos().then((allTodos) => {
-      updateTodos(allTodos);
+  updateTodos(updatedTodos) {
+    this.setState({
+      ...this.state,
+      todos: updatedTodos,
     });
   }
 
-  function updatePageTitle() {
-    document.title = `Es gibt ${todos.length} Todos`;
+  componentDidMount() {
+    serverFetchTodos().then((allTodos) => {
+      this.updateTodos(allTodos);
+    });
   }
 
-  function componentDidUpdate() {
-    updatePageTitle();
+  updatePageTitle() {
+    document.title = `Es gibt ${this.state.todos.length} Todos`;
   }
 
-  function addNewTodo(todoObj) {
+  componentDidUpdate() {
+    this.updatePageTitle();
+  }
+
+  addNewTodo(todoObj) {
     const optimisticTodos = [
-      ...todos,
+      ...this.state.todos,
       {
         ...todoObj,
         isOptimistic: true,
@@ -43,184 +48,77 @@ function App() {
       },
     ];
 
-    updateTodos(optimisticTodos);
+    this.updateTodos(optimisticTodos);
 
     serverAddTodo(todoObj)
-      .then(updatedTodos => updateTodos(updatedTodos));
+      .then(updatedTodos => this.updateTodos(updatedTodos));
   }
 
-  function setCompletionStateOfTodo(todoObj, isCompleted = false) {
+  setCompletionStateOfTodo(todoObj, isCompleted = false) {
     const updatedTodoObj = {
       ...todoObj,
       isCompleted,
     };
 
     serverUpdateTodo(todoObj.id, updatedTodoObj)
-      .then(updatedTodos => updateTodos(updatedTodos));
+      .then(updatedTodos => this.updateTodos(updatedTodos));
   }
 
-  function removeTodo(todoId) {
+  removeTodo(todoId) {
     serverRemoveTodo({ id: todoId })
-      .then(updatedTodos => updateTodos(updatedTodos));
+      .then(updatedTodos => this.updateTodos(updatedTodos));
   }
 
-  function openTodos() {
-    return todos.filter(({ isCompleted }) => isCompleted !== true);
+  get openTodos() {
+    return this.state.todos.filter(({ isCompleted }) => isCompleted !== true);
   }
 
-  function completedTodos() {
-    return todos.filter(({ isCompleted }) => isCompleted === true);
+  get completedTodos() {
+    return this.state.todos.filter(({ isCompleted }) => isCompleted === true);
   }
 
-  return (
-    <UserContext.Provider value="Mx Fisch">
-      <h1>Meine Todo App</h1>
+  render() {
+    return (
+      <UserContext.Provider value="Mx Fisch">
+        <h1>Meine Todo App</h1>
 
-      <TodoHeader todos={todos} />
+        <TodoHeader todos={this.state.todos} />
 
-      <TodoInput onAddTodo={(todoObj) => addNewTodo(todoObj)} />
+        <TodoInput onAddTodo={(todoObj) => this.addNewTodo(todoObj)} />
 
-      {todos.length > 0 && (
-        <table>
-          <thead>
-            <tr>
-              <th className="span-100">Todo</th>
-              <th className="span-100">Kategorie</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* Alle offenen Todos zuerst */}
-            <ListTodos
-              todos={openTodos()}
-              onSetCompletionState={(todoObj, completionState) =>
-                setCompletionStateOfTodo(todoObj, completionState)
-              }
-              onDeleteTodo={todoId => removeTodo(todoId)}
-            />
+        {this.state.todos.length > 0 && (
+          <table>
+            <thead>
+              <tr>
+                <th className="span-100">Todo</th>
+                <th className="span-100">Kategorie</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Alle offenen Todos zuerst */}
+              <ListTodos
+                todos={this.openTodos}
+                onSetCompletionState={(todoObj, completionState) =>
+                  this.setCompletionStateOfTodo(todoObj, completionState)
+                }
+                onDeleteTodo={todoId => this.removeTodo(todoId)}
+              />
 
-            {/* Dann die bereits abgeschlossenen */}
-            <ListTodos
-              todos={completedTodos()}
-              onSetCompletionState={(todoObj, completionState) =>
-                setCompletionStateOfTodo(todoObj, completionState)
-              }
-              onDeleteTodo={todoId => removeTodo(todoId)}
-            />
-          </tbody>
-        </table>
-      )}
-    </UserContext.Provider>
-  );
+              {/* Dann die bereits abgeschlossenen */}
+              <ListTodos
+                todos={this.completedTodos}
+                onSetCompletionState={(todoObj, completionState) =>
+                  this.setCompletionStateOfTodo(todoObj, completionState)
+                }
+                onDeleteTodo={todoId => this.removeTodo(todoId)}
+              />
+            </tbody>
+          </table>
+        )}
+      </UserContext.Provider>
+    );
+  }
 }
-
-// class App extends React.Component {
-//   state = {
-//     todos: [], // initally empty
-//   };
-
-//   updateTodos(updatedTodos) {
-//     this.setState({
-//       ...this.state,
-//       todos: updatedTodos,
-//     });
-//   }
-
-//   componentDidMount() {
-//     serverFetchTodos().then((allTodos) => {
-//       this.updateTodos(allTodos);
-//     });
-//   }
-
-//   updatePageTitle() {
-//     document.title = `Es gibt ${this.state.todos.length} Todos`;
-//   }
-
-//   componentDidUpdate() {
-//     this.updatePageTitle();
-//   }
-
-//   addNewTodo(todoObj) {
-//     const optimisticTodos = [
-//       ...this.state.todos,
-//       {
-//         ...todoObj,
-//         isOptimistic: true,
-//         id: randomId(), // satisfying reacts "key" requirement
-//       },
-//     ];
-
-//     this.updateTodos(optimisticTodos);
-
-//     serverAddTodo(todoObj)
-//       .then(updatedTodos => this.updateTodos(updatedTodos));
-//   }
-
-//   setCompletionStateOfTodo(todoObj, isCompleted = false) {
-//     const updatedTodoObj = {
-//       ...todoObj,
-//       isCompleted,
-//     };
-
-//     serverUpdateTodo(todoObj.id, updatedTodoObj)
-//       .then(updatedTodos => this.updateTodos(updatedTodos));
-//   }
-
-//   removeTodo(todoId) {
-//     serverRemoveTodo({ id: todoId })
-//       .then(updatedTodos => this.updateTodos(updatedTodos));
-//   }
-
-//   get openTodos() {
-//     return this.state.todos.filter(({ isCompleted }) => isCompleted !== true);
-//   }
-
-//   get completedTodos() {
-//     return this.state.todos.filter(({ isCompleted }) => isCompleted === true);
-//   }
-
-//   render() {
-//     return (
-//       <UserContext.Provider value="Mx Fisch">
-//         <h1>Meine Todo App</h1>
-
-//         <TodoHeader todos={this.state.todos} />
-
-//         <TodoInput onAddTodo={(todoObj) => this.addNewTodo(todoObj)} />
-
-//         {this.state.todos.length > 0 && (
-//           <table>
-//             <thead>
-//               <tr>
-//                 <th className="span-100">Todo</th>
-//                 <th className="span-100">Kategorie</th>
-//                 <th></th>
-//               </tr>
-//             </thead>
-//             <tbody>
-//               {/* Alle offenen Todos zuerst */}
-//               <ListTodos
-//                 todos={this.openTodos}
-//                 onSetCompletionState={(todoObj, completionState) =>
-//                   this.setCompletionStateOfTodo(todoObj, completionState)
-//                 }
-//                 onDeleteTodo={todoId => this.removeTodo(todoId)}
-//               />
-
-//               {/* Dann die bereits abgeschlossenen */}
-//               <ListTodos
-//                 todos={this.completedTodos}
-//                 onSetCompletionState={(todoObj, completionState) =>
-//                   this.setCompletionStateOfTodo(todoObj, completionState)
-//                 }
-//                 onDeleteTodo={todoId => this.removeTodo(todoId)}
-//               />
-//             </tbody>
-//           </table>
-//         )}
-//       </UserContext.Provider>
-//     );
-//   }
-// }
 
 ReactDOM.render(<App />, document.getElementById("app"));
