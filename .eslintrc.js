6379ab1db781a9518c29dcb3fa4e0526ba73a03a
David Lorenz <info@activenode.de>
2021-06-25 17:35:31 +0200

    eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,20 @@
 module.exports = {
-  "parser": "babel-eslint",
-  plugins: [
-    "react-hooks",
-  ],
+  parser: "@babel/eslint-parser",
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
   rules: {
-    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+    "react/prop-types": 0,
+    "no-unused-vars": "warn",
   },
 };
